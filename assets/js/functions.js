ac3bf923e1ed25f2b6c8bfd0e96e17799440364d
Nataniel Martin <nmartin@appstud.me>
2015-07-18 21:16:10 +0200

    add tags + skill design + scroll js method

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,10 @@
 $( document ).ready(function() {
 		$( ".aboutme" ).css( "-webkit-animation-duration", "0.3s");
+
+		$('.scrollTo').click( function() { // Au clic sur un élément
+			var page = $(this).attr('href'); // Page cible
+			var speed = 750; // Durée de l'animation (en ms)
+			$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+			return false;
+		});
 });
